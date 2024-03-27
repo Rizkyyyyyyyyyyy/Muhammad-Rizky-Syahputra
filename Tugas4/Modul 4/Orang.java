7dc76848c95d8eac2b63ab3450dc42b623dba158
Rizkyyyyyyyyyyy <129214795+Rizkyyyyyyyyyyy@users.noreply.github.com>
2024-03-27 21:52:40 +0800

    Add files via upload

---
+++ Tugas4/Modul 4/Orang.java
@@ -0,0 +1,12 @@
+public class Orang {
+    public String nama;
+    public Orang() {
+        this.nama = "Aminah";
+    }
+    public Orang(String nama) {
+        this.nama = nama;
+    }
+    public static void info() {
+        System.out.println("Class Orang");
+    }
+}
